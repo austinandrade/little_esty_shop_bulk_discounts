373290745c7636041320d52ff0bea5603fae94b2
austinandrade <austinmandrade@gmail.com>
2021-04-25 13:42:08 -0400

    Add bulk discounts resource under merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: 'application#welcome'
-  
+
   resources :merchant, only: [:show] do
+    resources :bulk_discounts
     resources :dashboard, only: [:index]
     resources :items, except: [:destroy]
     resources :item_status, only: [:update]
