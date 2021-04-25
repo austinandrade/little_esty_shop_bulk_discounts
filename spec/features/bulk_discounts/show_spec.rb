4578215d97b0366ed1080e2d5a44a72b64083a58
austinandrade <austinmandrade@gmail.com>
2021-04-25 19:17:36 -0400

    Add edit link on show page that redirects to edit action page

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -2,16 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'merchant bulk discounts show' do
   before :each do
-    @merchant_1 = Merchant.create!(name: 'Hair Care')
+    @merchant = Merchant.create!(name: 'Hair Care')
+    @bulk_discount = @merchant.bulk_discounts.create!(name: '25 percent off 10 items', percentage_discount: 25, quantity_threshold: 10)
 
-    @bulk_discount_1 = @merchant_1.bulk_discounts.create!(name: '25 percent off 10 items', percentage_discount: 25, quantity_threshold: 10)
-
-    visit merchant_bulk_discount_path(@merchant_1, @bulk_discount_1)
+    visit merchant_bulk_discount_path(@merchant, @bulk_discount)
   end
 
   it "displays bulk discount's attributes and which merchant it's for" do
-    expect(page).to have_content(@bulk_discount_1.name)
-    expect(page).to have_content(@bulk_discount_1.percentage_discount)
-    expect(page).to have_content(@bulk_discount_1.quantity_threshold)
+    expect(page).to have_content(@bulk_discount.name)
+    expect(page).to have_content(@bulk_discount.percentage_discount)
+    expect(page).to have_content(@bulk_discount.quantity_threshold)
+  end
+
+  it "clicks edit link and redirects to edit page" do
+    expect(page).to have_link("Edit")
+    click_link("Edit")
+
+    expect(page).to have_current_path("/merchant/#{@merchant.id}/bulk_discounts/#{@bulk_discount.id}/edit")
   end
 end
