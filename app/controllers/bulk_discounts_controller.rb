394ccc7c3e1932b387889a46f44417324efd3567
austinandrade <austinmandrade@gmail.com>
2021-04-25 13:43:20 -0400

    Add index and show functionality and variable verification

---
+++ app/controllers/bulk_discounts_controller.rb
@@ -0,0 +1,17 @@
+class BulkDiscountsController < ApplicationController
+  before_action :find_merchant, only: [:index]
+
+  def index
+    @bulk_discounts = @merchant.bulk_discounts
+  end
+
+  def show
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  private
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+end
---
+++ spec/features/bulk_discounts/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant bulk discounts index' do
+  before :each do
+    @merchant_1 = Merchant.create!(name: 'Hair Care')
+
+    @bulk_discount_1 = @merchant_1.bulk_discounts.create!(name: '25 percent off 10 items', percentage_discount: 25, quantity_threshold: 10)
+
+    visit merchant_bulk_discount_path(@merchant_1, @bulk_discount_1)
+  end
+
+  it "displays bulk discount's attributes and which merchant it's for" do
+    expect(page).to have_content(@bulk_discount_1.name)
+    expect(page).to have_content(@bulk_discount_1.percentage_discount)
+    expect(page).to have_content(@bulk_discount_1.quantity_threshold)
+    expect(page).to have_content(@bulk_discount_1.merchant.name)
+  end
+end
