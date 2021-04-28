17d73c6deb2250ed7a7a9f51c6067faaa81afb33
austinandrade <austinmandrade@gmail.com>
2021-04-27 22:31:56 -0400

    Fix associations

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -7,7 +7,7 @@ class BulkDiscount < ApplicationRecord
                             :quantity_threshold,
                             :merchant_id
   belongs_to :merchant
-  has_many :items, through: :merchants
+  has_many :items, through: :merchant
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
   has_many :customers, through: :invoices
